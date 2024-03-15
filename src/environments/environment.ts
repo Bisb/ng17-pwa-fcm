a8c191a0df8307860fdb2336d6e04744b89168ae
Guillaume Adam <guillaume@gadam.fr>
2024-03-15 12:18:22 +0100

    environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyBC9Zn-No5c-sPmKuGtQm5KjPLZDzAPU2A',
+    authDomain: 'ng-pwa-messaging.firebaseapp.c om',
+    projectId: 'ng-pwa-messaging',
+    storageBucket: 'ng-pwa-messaging.appspot.com',
+    messagingSenderId: '58471394982',
+    appId: '1:58471394982:web:2fe5acad6b02cffdba0e67',
+  },
+};
